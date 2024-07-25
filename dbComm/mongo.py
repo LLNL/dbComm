e2b1725e64f0cd6f26a6fce6dcc9e0de190d3342
caviness2 <caviness2@llnl.gov>
2024-07-24 18:37:38 -0700

    fixed issue if no username provided; added SSH compatibility

--- dbComm/mongo.py
+++ dbComm/mongo.py
@@ -11,13 +11,27 @@ import pickle
 from PIL import Image
 import pymongo
 import os
-import getpass
+from getpass import getpass, getuser
 import sys
+from contextlib import contextmanager
+from sshtunnel import SSHTunnelForwarder
+
 
 class Mongo:
     """Provide an interface to MongoDB."""
 
-    def __init__(self, host, port=27017, authentication=None, OUN=None, AD=None, timeout=5000):
+    def __init__(
+        self,
+        host,
+        port=27017,
+        authentication=None,
+        username=None,
+        password=None,
+        ssh=False,
+        ssh_host=None,
+        database=None,
+        timeout=5000,
+    ):
         """Establish connection to a MongoDB server.
 
         The `host` parameter can be a full `mongodb URI
@@ -29,23 +43,41 @@ class Mongo:
             mongodb URI.
           - port (optional): port number on which to connect
           - authentication (optional): type of authentication
-          - OUN (optional): username to connect to DB
-          - AD (optional): password to connect to DB
-          """
+          - username (optional): username to connect to DB
+          - password (optional): password to connect to DB
+        """
 
         self.timeout = timeout
-        
-        if 'mongodb://' in host:
+        self.server = None
+        if username:
+            self.username = username
+        else:
+            self.username = getuser()
+        if "mongodb://" in host:
             try:
                 self.newConn(host)
             except pymongo.errors.ServerSelectionTimeoutError:
-                raise Exception('Server timeout. Check connection details.')
+                raise Exception("Server timeout. Check connection details.")
+        elif ssh == True:
+            self.server = SSHTunnelForwarder(
+                (ssh_host, 22),
+                ssh_username=input("LC Username: "),
+                ssh_password=getpass("LC Password (VPN/CZ PIN + Token): "),
+                remote_bind_address=(host, port),
+                allow_agent=False,
+            )
+            self.server.start()
+            self.dbClient = pymongo.MongoClient(
+                f"mongodb://{username}:{password}@127.0.0.1/{database}?ssl=true&tlsAllowInvalidCertificates=true",
+                port=self.server.local_bind_port,
+            )
+            self.dbList = self.dbClient.list_database_names()
         elif authentication == None:
             try:
-                self.newConn(f'{host}:{port}')
+                self.newConn(f"{host}:{port}")
             except pymongo.errors.ServerSelectionTimeoutError:
-                raise Exception('Server timeout. Check connection details.')
-        elif authentication == 'LDAP':
+                raise Exception("Server timeout. Check connection details.")
+        elif authentication == "LDAP":
             auth = False
             # attempts = 0
             # while not auth:
@@ -54,42 +86,41 @@ class Mongo:
             #         raise Exception('Too many failed password attempts.')
             #         break
             try:
-                if OUN:
-                    self.OUN = OUN
-                else:
-                    self.OUN = getpass.getuser()
                 # to work in PyCharm, 'Edit Configurations' and tick the 'Emulate terminal' box
                 # otherwise, the entered password will be printed to the console
-                if AD:
+                if password:
                     pass
                 elif sys.stdin.isatty():
-                    AD = getpass.getpass('Enter AD: ')
+                    password = getpass("Enter AD: ")
                 else:
-                    print('Enter AD: ')
-                    AD = sys.stdin.readline().rstrip()
+                    print("Enter AD: ")
+                    password = sys.stdin.readline().rstrip()
                 if type(host) is list:
-                    insert = ''
+                    insert = ""
                     for h in host:
-                        insert += f'{h}:{port},'
+                        insert += f"{h}:{port},"
                     insert = insert[:-1]  # removes trailing comma
-                    uri = f'mongodb://{self.OUN}:{AD}@{insert}/?authMechanism=PLAIN&replicaSet=ame&authSource=%24external&ssl=true'
+                    uri = f"mongodb://{self.username}:{password}@{insert}/?authMechanism=PLAIN&replicaSet=ame&authSource=%24external&ssl=true"
                 else:
-                    uri = f'mongodb://{self.OUN}:{AD}@{host}:{port}/tls=true?authMechanism=PLAIN&' \
-                          'replicaSet=ame&readPreference=primary&authSource=%24external&directConnection=true&ssl=true'
+                    uri = (
+                        f"mongodb://{self.username}:{password}@{host}:{port}/tls=true?authMechanism=PLAIN&"
+                        "replicaSet=ame&readPreference=primary&authSource=%24external&directConnection=true&ssl=true"
+                    )
                 self.newConn(uri)
                 auth = True
-                print(f'Connected to {host}')
+                print(f"Connected to {host}")
             except pymongo.errors.OperationFailure:  # Authentication Error
-                print('Authentication Error. Try again.')
-                AD = None
+                print("Authentication Error. Try again.")
+                password = None
             except pymongo.errors.ServerSelectionTimeoutError:
-                raise Exception('Server timeout. Check connection details.')
+                raise Exception("Server timeout. Check connection details.")
         else:
-            print('Authentication method not set up yet. Contact admin')
+            print("Authentication method not set up yet. Contact admin")
 
-    '''
+    """
     Connection block
-    '''
+    """
+
     def newConn(self, host):
         """Connect to database and establish collections
 
@@ -121,11 +152,12 @@ class Mongo:
             self.fs = gridfs.GridFS(self.db)
             self.collList = self.db.list_collection_names()
         else:
-            print(db_name, 'is not one of the databases on the server.')
+            print(db_name, "is not one of the databases on the server.")
 
-    '''
+    """
     This Section is actions on ALL collections
-    '''
+    """
+
     def getDBRecByID(self, RecID):
         """Search all collections for a record _id
 
@@ -138,7 +170,7 @@ class Mongo:
         fColl, fRec = None, None
         for x in self.collList:
             coll = self.db[f"{x}"]
-            rec = coll.find_one({'_id': ObjectId(RecID)})
+            rec = coll.find_one({"_id": ObjectId(RecID)})
             if rec:
                 fColl = x
                 fRec = rec
@@ -153,60 +185,71 @@ class Mongo:
         Return:
             retList: list of the records with the matching field"""
         if type(field) is not dict:
-            Exception('search field must be type <dict>')
+            Exception("search field must be type <dict>")
         else:
             retList = list()
-            if 'system.profile' in self.collList:
-                self.collList.remove('system.profile')
+            if "system.profile" in self.collList:
+                self.collList.remove("system.profile")
             for x in self.collList:
                 coll = self.db[f"{x}"]
                 for rec in coll.find(field):
                     retList.append({coll.name: rec})
             return retList
 
-    '''
+    """
     This Section is actions on records in a specified collection
-    '''
+    """
+
     def getRecord(self, collection, field):
-        '''Get a record given its collection and _id
+        """Get a record given its collection and _id
 
         Parameters:
             collection: the collection of the desired record
             field: the _id or a key-value identifier of the desired record
 
         Return:
-            retRec: the record corresponding to the recID'''
+            retRec: the record corresponding to the recID"""
 
         if type(field) is str or type(field) is bson.objectid.ObjectId:
             field = {"_id": ObjectId(field)}
         elif type(field) is not dict:
-            raise Exception('Invalid field. Must be either an ObjectId or a dictionary')
+            raise Exception("Invalid field. Must be either an ObjectId or a dictionary")
 
         if collection in self.collList:
-                retRec = self.db[collection].find_one(field)
+            retRec = self.db[collection].find_one(field)
         else:
             retRec = None
-            print('the collection \'' + collection + '\' does not exist in the database ' + str(self.db.name))
+            print(
+                "the collection '"
+                + collection
+                + "' does not exist in the database "
+                + str(self.db.name)
+            )
         return retRec
 
     def getRecords(self, collection):
-        '''Get all records in a collection
+        """Get all records in a collection
 
         Parameters:
             collection: the collection of the desired record
 
         Return:
-            retStr: list of the records in the collection'''
+            retStr: list of the records in the collection"""
         retStr = list()
         if collection in self.collList:
             for x in self.db[collection].find():
                 retStr.append(x)
         else:
-            print('the collection \'' + collection + '\' does not exist in the database ' + str(self.db.name))
+            print(
+                "the collection '"
+                + collection
+                + "' does not exist in the database "
+                + str(self.db.name)
+            )
         return retStr
 
     def updateRecord(self, collection, field, updateVals, updateType):
-        '''Update a record with a set of values given a collection and _id
+        """Update a record with a set of values given a collection and _id
 
         Parameters:
             collection: the collection of the desired record
@@ -217,32 +260,32 @@ class Mongo:
 
         Return:
             None
-        '''
+        """
         if type(field) is str or type(field) is bson.objectid.ObjectId:
             field = {"_id": ObjectId(field)}
         elif type(field) is not dict:
-            raise Exception('Invalid field. Must be either an ObjectId or a dictionary')
+            raise Exception("Invalid field. Must be either an ObjectId or a dictionary")
 
         if collection in self.collList:
             retRec = self.db[collection].find_one(field)
-        if updateType == 'set':  # overrides the values
-            self.db[collection].update_one(field, {'$set': updateVals})
-        elif updateType == 'push':  # appends the values to an array
-            self.db[collection].update_one(field, {'$push': updateVals})
+        if updateType == "set":  # overrides the values
+            self.db[collection].update_one(field, {"$set": updateVals})
+        elif updateType == "push":  # appends the values to an array
+            self.db[collection].update_one(field, {"$push": updateVals})
 
     def newRecord(self, collection, **kwargs):
-        '''Create a record in a given collection (with optional contents)
+        """Create a record in a given collection (with optional contents)
 
-                Parameters:
-                    collection: the collection of the desired record
-                    kwargs (optional): key-value pairs of data to add to the record
+        Parameters:
+            collection: the collection of the desired record
+            kwargs (optional): key-value pairs of data to add to the record
 
-                Return:
-                    _id of created record
-                '''
+        Return:
+            _id of created record
+        """
 
         newRec = {
-            "userName": self.OUN,
+            "userName": self.username,
             "CreateTime": datetime.datetime.now(),
         }
         newRec.update(kwargs)
@@ -250,78 +293,92 @@ class Mongo:
         return docID.inserted_id
 
     def deleteRecords(self, collection):
-        '''Delete all records in a given collection
+        """Delete all records in a given collection
 
-                Parameters:
-                    collection: the collection to be cleared
+        Parameters:
+            collection: the collection to be cleared
 
-                Return:
-                    None
-                '''
+        Return:
+            None
+        """
         if collection in self.collList:
             x = collection.delete_many({})
             print(x.deleted_count, " documents deleted.")
         else:
-            print('the collection \'' + collection + '\' does not exist in the database ' + str(self.db.name))
+            print(
+                "the collection '"
+                + collection
+                + "' does not exist in the database "
+                + str(self.db.name)
+            )
 
     def dropCollection(self, collection):
-        '''Delete a given collection
+        """Delete a given collection
 
-                Parameters:
-                    collection: the collection to be deleted
+        Parameters:
+            collection: the collection to be deleted
 
-                Return:
-                    None
-                '''
+        Return:
+            None
+        """
         x = collection.drop()
         if x:
-            print(collection + ' collection dropped successfully')
+            print(collection + " collection dropped successfully")
         else:
-            print('the collection \'' + collection + '\' does not exist in the database ' + str(self.db.name))\
+            print(
+                "the collection '"
+                + collection
+                + "' does not exist in the database "
+                + str(self.db.name)
+            )
 
-
-    '''
+    """
     Methods for handling files in the database with gridFS
-    '''
+    """
+
     def putFile(self, filepath, **kwargs):
-        '''Put a file in GridFS storage
+        """Put a file in GridFS storage
 
-                Parameters:
-                    filepath: path to the file to be uploaded
-                    kwargs (optional): additional fields to be added to the record
+        Parameters:
+            filepath: path to the file to be uploaded
+            kwargs (optional): additional fields to be added to the record
 
-                Return:
-                    _id of the file record
-                '''
+        Return:
+            _id of the file record
+        """
         return self.fs.put(filepath, **kwargs)
 
     def getFile(self, fileID):
-        '''Get a file from GridFS storage
+        """Get a file from GridFS storage
 
-                        Parameters:
-                            fileID: _id of the file record
+        Parameters:
+            fileID: _id of the file record
 
-                        Return:
-                            the file corresponding to the _id (as returned by its .read() function)
-                        '''
+        Return:
+            the file corresponding to the _id (as returned by its .read() function)
+        """
         if type(fileID) is str:
             fileID = ObjectId(fileID)
         return self.fs.get(fileID).read()
 
     def deleteFile(self, fileID):
-        '''Delete a file from GridFS storage
+        """Delete a file from GridFS storage
 
-           Parameters:
-               fileID: _id of the file record
+        Parameters:
+            fileID: _id of the file record
 
-           Return:
-               None
-           '''
+        Return:
+            None
+        """
         self.fs.delete(fileID)
+
+    def stop(self):
+        """If an SSH connection is done, this is required to stop the connection."""
+        if self.server:
+            self.server.stop()
 
 
 if __name__ == "__main__":
-    db = Mongo('localhost')
+    db = Mongo("localhost")
     print(db.getDBs())
-
-
+    db.stop()
